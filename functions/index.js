ce85a33a381ff78091c0865e72c41d25dbdc869b
Patrick Kenny <patrick@didey.net>
2021-10-07 02:15:04 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,15 @@ const { searchAmazon } = require('unofficial-amazon-search');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.search = functions.https.onCall(async (data, context) => {
-	let results = await searchAmazon(data.search).catch((error) => {
-		functions.logger.error(error);
-	});
-	functions.firestore.namespace();
+exports.search = functions.https.onCall((data, context) => {
 	admin.firestore().collection('searches').add({
 		search: data.search,
 	});
-	functions.logger.info('Search results:');
-	functions.logger.info(results);
-	let url = results.searchResults[0].productUrl;
-	return `https://amazon.com${url}${url.endsWith('/') ? '?' : '&'}tag=fast1020-20`;
+	return new Promise(async (resolve, reject) => {
+		let results = await searchAmazon(data.search).catch((error) => {
+			functions.logger.error(error);
+		});
+		let url = results.searchResults[0].productUrl;
+		resolve(`https://amazon.com${url}${url.endsWith('/') ? '?' : '&'}tag=fast1020-20`);
+	});
 });

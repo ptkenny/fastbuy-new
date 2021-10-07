e80eed75e721a323ae403ae3e7a843c2532048c6
Patrick Kenny <patrick@didey.net>
2021-10-07 03:20:08 -0400

    Adds show latest searches, adds a timestamp to each search in the database.

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,12 @@ admin.initializeApp();
 exports.search = functions.https.onCall((data, context) => {
 	admin.firestore().collection('searches').add({
 		search: data.search,
+		timestamp: admin.firestore.FieldValue.serverTimestamp(),
 	});
 	return new Promise(async (resolve, reject) => {
 		let results = await searchAmazon(data.search).catch((error) => {
 			functions.logger.error(error);
+			reject(error);
 		});
 		let url = results.searchResults[0].productUrl;
 		resolve(`https://amazon.com${url}${url.endsWith('/') ? '?' : '&'}tag=fast1020-20`);
--- src/index.js
+++ src/index.js
@@ -2,19 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '@fontsource/roboto/400.css';
-import { Button, Grid, Stack, TextField, Typography } from '@mui/material';
+import { Button, Grid, LinearProgress, Stack, TextField, Typography, Box } from '@mui/material';
 
 import { getFunctions, httpsCallable } from 'firebase/functions';
+import { getFirestore, collection, query, getDocs, orderBy, limit } from 'firebase/firestore';
 import startFirebase from './firebase';
+
+startFirebase();
+const q = query(collection(getFirestore(), 'searches'), orderBy('timestamp', 'desc'), limit(3));
 
 function MainView(props) {
 	let [search, setSearch] = React.useState('');
+	let [loading, setLoading] = React.useState(false);
+	let [latest, setLatest] = React.useState(false);
 
 	React.useEffect(() => {
-		startFirebase();
+		async function fetchQuery() {
+			const querySnapshot = await getDocs(q);
+			await setLatest(
+				querySnapshot.docs.map((doc, i) => (
+					<Box key={i}>
+						<hr />
+						<Typography variant="h6" textAlign="center">
+							{doc.data().search}
+						</Typography>
+					</Box>
+				))
+			);
+		}
+		fetchQuery();
 	}, []);
 
 	let searchAmazon = () => {
+		setLoading(true);
 		const functions = getFunctions();
 		httpsCallable(
 			functions,
@@ -22,19 +42,33 @@ function MainView(props) {
 		)({ search: search }).then((result) => {
 			let url = result.data;
 			window.location.href = url;
+			setLoading(false);
 		});
 	};
 
 	return (
-		<Grid container justifyContent="center" alignItems="center" justifySelf="center">
-			<Stack direction="column" spacing={5}>
-				<Typography variant="h1">FastBuy</Typography>
-				<TextField label="Search" variant="outlined" onChange={(event) => setSearch(event.target.value)} />
-				<Button variant="contained" onClick={searchAmazon}>
-					Search
-				</Button>
-			</Stack>
-		</Grid>
+		<>
+			<Grid container justifyContent="center" alignItems="center">
+				<Stack direction="column" spacing={3}>
+					<Stack direction="column" spacing={5}>
+						<Typography variant="h1">FastBuy</Typography>
+						<TextField
+							label="Search"
+							variant="outlined"
+							onChange={(event) => setSearch(event.target.value)}
+						/>
+						<Button variant="contained" onClick={searchAmazon}>
+							Search
+						</Button>
+						{loading && <LinearProgress />}
+					</Stack>
+					<Stack direction="column" spacing={2}>
+						<Typography variant="h3">Latest Searches</Typography>
+						{latest && latest}
+					</Stack>
+				</Stack>
+			</Grid>
+		</>
 	);
 }
 
